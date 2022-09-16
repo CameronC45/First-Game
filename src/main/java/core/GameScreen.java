11eb90dc4b876f52d4ab822de33d9bd2ae496d04
Cameron045 <cameron.cooney.2019@mumail.ie>
2022-09-16 16:24:03 +0100

    Creating background

--- src/main/java/core/GameScreen.java
+++ src/main/java/core/GameScreen.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import helper.TileMapHelper;
 import org.lwjgl.opengl.GL20;
 
 import static helper.Constants.PPM;
@@ -19,11 +21,18 @@ public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
+
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private TileMapHelper tileMapHelper;
+
     public GameScreen(OrthographicCamera camera){
         this.camera = camera;
         this.batch = new SpriteBatch();
         this.world = new World(new Vector2(0,0), false);
         this.box2DDebugRenderer = new Box2DDebugRenderer();
+
+        this.tileMapHelper = new TileMapHelper(this);
+        this.orthogonalTiledMapRenderer = tileMapHelper.setupMap();
     }
 
     private void update() {
@@ -31,6 +40,7 @@ public class GameScreen extends ScreenAdapter {
         cameraUpdate();
 
         batch.setProjectionMatrix(camera.combined);
+        orthogonalTiledMapRenderer.setView(camera);
 
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
             Gdx.app.exit();
@@ -49,9 +59,15 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        orthogonalTiledMapRenderer.render();
+
         batch.begin();
         // render objects
         batch.end();
         box2DDebugRenderer.render(world, camera.combined.scl(PPM));
+    }
+
+    public World getWorld() {
+        return world;
     }
 }
